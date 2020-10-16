00e7155b40d4aa6be78d68cdf902775883cb3831
Opinsys <dev@opinsys.fi>
2020-10-16 09:53:55 +0300

    Basic curl operations added
    - untested on server

--- src/apiwatcher.sh
+++ src/apiwatcher.sh
@@ -3,13 +3,41 @@ mediadir="/media/usb1"
 opinsysdir="$mediadir/.opinsys"
 cmd_file="$opinsysdir/cmd"
 output_file="$opinsysdir/output"
-
+cookie_file="$opinsysdir/cookie.txt"
 
 # Define function to parse cmd-file
-# outputs cmd as 
+# outputs cmd as $cmd_cmd, $cmd_arg1, $cmd_arg2
 read_command() {
     cmd_whole=`cat $cmd_file`
-    cmd_cmd=`echo $cmd_whole | {read a _}`
+    # split $cmd_whole to words
+    IFS=' '
+    read -a strarr <<< "$cmd_whole"
+    case ${#strarr[*]} in
+        0)
+            cmd_cmd=""
+            cmd_arg1=""
+            cmd_arg2=""
+        ;;
+        1)
+            cmd_cmd=${strarr[0]}
+            cmd_arg1=""
+            cmd_arg2=""
+        ;;
+        2)
+            cmd_cmd=${strarr[0]}
+            cmd_arg1=${strarr[1]}
+            cmd_arg2=""
+        ;;
+        *)
+            cmd_cmd=${strarr[0]}
+            cmd_arg1=${strarr[1]}
+            cmd_arg2=${strarr[2]}
+        ;;
+    esac
+}
+
+init_session() {
+    curl -b $cookie_file -c $cookie_file 'http://localhost'
 }
 
 stamp_execution() {
@@ -22,27 +50,51 @@ execute_ping() {
     stamp_execution "ping"
 }
 
+uncrypt_exam_singlekey() {
+    local decryptkey=$1
+    output_decrypt=`curl 'http://localhost/decrypt-exam' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Accept-Language: fi-FI,fi;q=0.8,en-US;q=0.5,en;q=0.3' --compressed -H 'Referer: http://localhost/' -H 'Content-Type: application/json; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' -H 'Cookie: supoLang=fin' --data '{"decryptPassword":"$decryptkey"}'`
+
+}
+
 # Uncrypt exam
 # @params $1 filename containing decrypt keystrings
 # on false keys lists false keys on global var $falsekeys
 uncrypt_exam() {
-    local examdecryptfile=$1
+    local examdecryptfile=`realpath $mediadir/$1`
     while read $line; do 
-
+        uncrypt_exam_singlekey $line
+        # TODO handle output
     done < $examdecryptfile
+    # TODO handle result
+
+}
+
+loadexam() {
+    local examfile=`realpath $mediadir/$1`
+    output=`curl -F 'examZip=@$examfile' 'http://localhost/load-exam' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' -H 'Accept: */*' -H 'Accept-Language: fi-FI,fi;q=0.8,en-US;q=0.5,en;q=0.3' --compressed -H 'Referer: http://localhost/' -H 'X-Requested-With: XMLHttpRequest' -b $cookie_file -c $cookie_file`
+}
+
+startexam() {
+    output_start=`curl 'http://localhost/start-exam' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Accept-Language: fi-FI,fi;q=0.8,en-US;q=0.5,en;q=0.3' --compressed -H 'Referer: http://localhost/' -H 'Content-Type: application/json; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' -H 'Cookie: supoLang=fin' --data '' -b $cookie_file -c $cookie_file`
 }
 
 # Executes loadexam
 execute_loadexam() {
     local newexamfile=$1
     local examdecryptfile=$2
-
+    init_session
+    loadexam $newexamfile
+    # TODO handle output
+    uncrypt_exam $examdecryptfile
 }
 
 read_command
 
-if [ $cmd_cmd -eq "ping" ] ; then
-    execute_ping
-elif [ $cmd_cmd -eq "load-exam" ] ; then
-    execute_loadexam $cmd_arg1 $cmd_arg2
-elif [ $cmd_cmd ] ; then
+case $cmd_cmd in
+    "ping")
+        execute_ping
+        ;;
+    "load-exam")
+        execute_loadexam $cmd_arg1 $cmd_arg2
+        ;;
+    esac    
--- src/extractlauncher.sh
+++ src/extractlauncher.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# KTP-API-asennuspaketti
+# (c) Opinsys Oy, 2020
 
 echo "Puretaan asennuspakettia"
 
