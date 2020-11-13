417e7639a726cf32844bb44e1cc7d43c6b25514d
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2020-11-13 13:07:31 +0200

    Fix installer-background.sh scripts.
    (There are currently two... these tried to do things with sudo
    but that is not how shell works).

--- src/platforms/SERVER2041X/installer-background.sh
+++ src/platforms/SERVER2041X/installer-background.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
 # Alters background image
+
+set -eu
+
 background_current_file=/usr/share/images/digabios-backgrounds/background.svg
 background_versiontag_file=/usr/share/images/digabios-backgrounds/background-versiontag.svg
 background_backup_file=/usr/share/images/digabios-backgrounds/background-bak.svg
 
-[[ -f "$background_backup_file" ]] || sudo cp "$background_versiontag_file" "$background_backup_file"
-sudo cat "$background_current_file" | awk '{ sub(/#385e77/,"#38775e"); print}' > "$background_current_file"
-sudo cat "$background_versiontag_file" | awk '{ sub(/@VERSION/,"@VERSION - AUTOMAATTI"); print }' | awk '{ sub(/#385e77/,"#38775e"); print}' > "$background_versiontag_file"
+if [[ ! -f "$background_backup_file" ]]; then
+  sudo cp "$background_versiontag_file" "$background_backup_file"
+fi
+
+sed 's/#385e77/#38775e/' "$background_current_file" \
+  | sudo tee "$background_current_file" > /dev/null
+sed 's/@VERSION/@VERSION - AUTOMAATTI/; s/#385e77/#38775e/' "$background_versiontag_file" \
+  | sudo tee "$background_versiontag_file" > /dev/null
