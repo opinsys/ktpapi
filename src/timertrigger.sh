fff5ee5f1108402b08fa474d183d6cfb421538d6
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2020-11-16 13:59:44 +0200

    Rewrite some parts of "timertrigger".

--- src/timertrigger.sh
+++ src/timertrigger.sh
@@ -1,45 +1,39 @@
-#!/bin/bash
+#!/bin/sh
+
 # Watches whether commandfile has changed
 
-set -u
+set -eu
 
-cmd_file="/media/usb1/.opinsys/cmd"
-stamp_file="/media/usb1/.opinsys/stampf"
-lockfile_file="/media/usb1/.opinsys/.cmd-lock"
-handler_script="/home/digabi/opinsys/apiwatcher.sh"
-output_lockfile="/media/usb1/.opinsys/.cmd-in-progress"
-# Max runtime script is allowed prestige is seconds, if 0, concurrence not detected/minded
-max_runtime=600
+cmd_file='/media/usb1/.opinsys/cmd'
+lockfile_file='/media/usb1/.opinsys/.cmd-lock'
+output_lockfile='/media/usb1/.opinsys/.cmd-in-progress'
+stamp_file='/media/usb1/.opinsys/.cmd-timestamp'
 
-debug_output() {
-    echo "$@"
-}
+handler_script='/home/digabi/opinsys/apiwatcher.sh'
 
-debug_output "Opinsys - Timer triggered"
+echo 'Opinsys - Timer triggered'
 
-lastchange=`stat -c %Y "${cmd_file}" 2> /dev/null || echo 0`
-lastchangeHandled=`cat "${stamp_file}" 2> /dev/null || echo 0`
-debug_output "Variables" $lastchange $lastchangeHandled
-if [[ ${lastchange} != ${lastchangeHandled} ]]; then
-    debug_output "Opinsys - Change detected"
-    echo "${lastchange}" > "${stamp_file}"
-    # enter lock
-    while [[ -f "${lockfile_file}" ]]; do
-        debug_output "Opinsys - In lock"
-        lockedAt=`cat "${lockfile_file}"`
-        if [[ lockedAt+max_runtime -le `date +%s` ]]; then
-            debug_output "Override lock"
-            break
-        fi
-        sleep 1
-    done
-    date +%s > "${lockfile_file}"
-    debug_output "Opinsys - Running script"
+exitstatus=0
 
-    "${handler_script}"
-    # release locks
-    rm -rf "${output_lockfile}"
-    rm -rf "${lockfile_file}"
-    debug_output "Opinsys - Lock released"
+(
+  # this to prevent concurrent events happening from timer
+  if ! flock -nx 9; then
+    echo 'Could not get an execution lock'
+    exit 1
+  fi
 
-fi
+  lastchange=$(stat -c %Y "$cmd_file" 2> /dev/null || echo 0)
+  lastchangeHandled=$(cat "$stamp_file" 2> /dev/null || echo 0)
+
+  if [ "$lastchange" != "$lastchangeHandled" ]; then
+    printf "%s\n" "$lastchange" > "$stamp_file"
+    echo 'Opinsys - Change detected, running script'
+
+    "$handler_script" || exitstatus=$?
+
+    echo 'Opinsys - script done, releasing output lockfile'
+    rm -f "$output_lockfile"
+  fi
+) 9> "$lockfile_file"
+
+exit $exitstatus
